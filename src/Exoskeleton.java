1f0a05c6a86ec499490301a4be9717d07b186bcd
Noahjb4 <noahreed1@gmail.com>
2022-03-29 03:30:00 -0400

    Monster Set up

---
+++ src/Exoskeleton.java
@@ -0,0 +1,14 @@
+public class Exoskeleton extends Monster{
+    /**
+     * Constructor creates a Monster with a name, hp, and level
+     *
+     * @param n   name of Monster
+     * @param l   level of Monster
+     * @param dmg damage of Monster
+     * @param def defense of Monster
+     * @param dc  dodgeChance of Monster
+     */
+    public Exoskeleton(String n, int l, int dmg, int def, int dc) {
+        super(n, l, dmg, def, dc);
+    }
+}
